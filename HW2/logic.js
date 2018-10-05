34a2fb6eb9dbf248d60687a4c9a184d8585d5bc9
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-05 09:40:41 -0700

    added some input validation

--- HW2/logic.js
+++ HW2/logic.js
@@ -1,15 +1,19 @@
 
 
 $("form").submit(function(event){
-    
+    //key variables used
     var nums = $("input");
     var hitnums = [0,0,0];
     var hitsTotal = 0;
-    var hitsClose = 0;
     
+    //alert if all inputs aren't different
+    if(nums[0].value == nums[1].value || nums[0].value ==nums[2].value || nums[1].value == nums[2].value){
+        alert("Please enter 3 different numbers.");
+        return;
+    }
+        
     
     var i;
-    
     for(i = 0; i < 3; i++){
         hitnums[i] = Math.floor(Math.random() * 9);
     }
@@ -19,7 +23,7 @@ $("form").submit(function(event){
     
     
     //added list
-    $("div#result").after("<h5>Generated Numbers: </h5><ul><li>" + hitnums[0] + "</li><li>" + hitnums[1] + "</li><li>" + hitnums[2] + "</li></ul> <h5>Hits Total:"+hitsTotal+"</h5><h5>CloseHits Total(1 off):"+hitsClose+"</h5>");
+    $("div#result").after("<h5>Generated Numbers: </h5><ul><li>" + hitnums[0] + "</li><li>" + hitnums[1] + "</li><li>" + hitnums[2] + "</li></ul> <h5>Hits Total:"+hitsTotal+"</h5>");
 
     //alert(nums[0].value);
     event.preventDefault();
