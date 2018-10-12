d1ee8f022415cdf1fdd745979f932a476c5ec743
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-11 19:07:07 -0700

    added comments

--- HW3/BinaryCreate/BinaryCreate/Node.cs
+++ HW3/BinaryCreate/BinaryCreate/Node.cs
@@ -1,9 +1,17 @@
-﻿using System;
-
+﻿/// <summary>
+/// Singly linked node class
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Node<T>
 {
     public T data;
     public Node<T> next;
+
+    /// <summary>
+    /// Singly linked node constructor
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="next"></param>
     public Node(T data, Node<T> next)
 	{
         this.data = data;
