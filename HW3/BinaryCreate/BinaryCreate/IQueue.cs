d6ee702e0e42769961dab44e573cfa4339b42b29
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-11 19:09:38 -0700

    added comments to IQueue file

--- HW3/BinaryCreate/BinaryCreate/IQueue.cs
+++ HW3/BinaryCreate/BinaryCreate/IQueue.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 
+/// <summary>
+/// A FIFO queue interface.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 interface IQueue<T>
 {
+
+    /// <summary>
+    /// Add an element to the rear of the queue
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns>The element that was enqueued</returns>
     T Push(T element);
 
-    //need exception
+    /// <summary>
+    /// Remove the front element from Queue.
+    /// </summary>
+    /// <returns>The element that was removed </returns>
     T Pop();
 
+    /// <summary>
+    /// Test if the queue is empty
+    /// </summary>
+    /// <returns>True if queue is empty and false otherwise</returns>
     bool IsEmpty();
 }
