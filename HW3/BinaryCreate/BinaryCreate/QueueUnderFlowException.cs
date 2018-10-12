0742eb9fc2767e4d13ff69a349ec3c5fdf876d71
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-11 19:15:43 -0700

    added comments to QueueUnderFlowException

--- HW3/BinaryCreate/BinaryCreate/QueueUnderFlowException.cs
+++ HW3/BinaryCreate/BinaryCreate/QueueUnderFlowException.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-//runtmeEx fix
+/// <summary>
+/// A custom unchecked exception to represent situations where an illegal operation was performed on an empty queue.
+/// </summary>
 public class QueueUnderFlowException : SystemException
 {
+    /// <summary>
+    /// Constructor
+    /// </summary>
 	public QueueUnderFlowException() : base()
 	{
 	}
 
+    /// <summary>
+    /// Contructor with message
+    /// </summary>
+    /// <param name="message"></param>
     public QueueUnderFlowException(string message) : base(message)
     {
     }
