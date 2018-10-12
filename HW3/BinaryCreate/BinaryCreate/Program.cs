c1b1f22ac9f309261ea20a5a38c1ecee9ca71b97
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-11 23:18:13 -0700

    added content to Program.cs

--- HW3/BinaryCreate/BinaryCreate/Program.cs
+++ HW3/BinaryCreate/BinaryCreate/Program.cs
@@ -1,20 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace BinaryCreate
 {
     class Program
     {
+        static LinkedList<string> GeneratedBinaryRepresentation(int n)
+        {
+            LinkedQueue<StringBuilder> q = new LinkedQueue<StringBuilder>();
+            LinkedList<string> output = new LinkedList<string>();
+
+            if(n < 1)
+            {
+                return output;
+            }
+
+            q.Push(new StringBuilder("1"));
+
+            while (n-- > 0)
+            {
+                StringBuilder sb = q.Pop();
+                output.AddLast(sb.ToString());
+
+                StringBuilder sbc = new StringBuilder(sb.ToString());
+
+                sb.Append('0');
+                q.Push(sb);
+
+                sbc.Append('1');
+                q.Push(sbc);
+            }
+            return output;
+        }
         static void Main(string[] args)
         {
-            //test Node
-            Node<int> the = new Node<int>(5, null);
-            the.data = 5;
-            Console.WriteLine(the.data);
-            Console.ReadLine();
+            int n = 10;
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Please invoke with the max value to print binary up to, like this:");
+                Console.WriteLine("\tjava? Main 12");
+                return;
+            }
+            try
+            {
+                n = Int32.Parse(args[0]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("I'm sorry, I can't understand the number: " + args[0]);
+                return;
+            }
+            LinkedList<string> output = GeneratedBinaryRepresentation(n);
+
+            int maxLength = output;
+            for(string s : output)
+            {
+                for(int i = 0; i < maxLength - s.Length(); ++i)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(s);
+            }
         }
     }
 }
