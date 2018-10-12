f744996ae90d85010ebed43f59bbfcb5d56da304
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-12 15:37:50 -0700

    added more comments to IQueue.cs

--- HW3/BinaryCreate/BinaryCreate/IQueue.cs
+++ HW3/BinaryCreate/BinaryCreate/IQueue.cs
@@ -8,21 +8,21 @@ interface IQueue<T>
 {
 
     /// <summary>
-    /// Add an element to the rear of the queue
+    /// Add an element to the rear of the queue.
     /// </summary>
-    /// <param name="element"></param>
-    /// <returns>The element that was enqueued</returns>
+    /// <param name="element"> The value that will be placed into the LinkedQueue. </param>
+    /// <returns> The element that was enqueued. </returns>
     T Push(T element);
 
     /// <summary>
     /// Remove the front element from Queue.
     /// </summary>
-    /// <returns>The element that was removed </returns>
+    /// <returns> The element that was removed. </returns>
     T Pop();
 
     /// <summary>
-    /// Test if the queue is empty
+    /// Test if the queue is empty.
     /// </summary>
-    /// <returns>True if queue is empty and false otherwise</returns>
+    /// <returns> True if queue is empty and false otherwise. </returns>
     bool IsEmpty();
 }
