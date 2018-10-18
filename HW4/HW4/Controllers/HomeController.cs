0cb759974b4ddc03556524da75fe46fdb0182bd3
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-17 18:01:45 -0700

    added converter related content to index

--- HW4/HW4/Controllers/HomeController.cs
+++ HW4/HW4/Controllers/HomeController.cs
@@ -13,18 +13,11 @@ namespace HW4.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Converter()
         {
-            ViewBag.Message = "Your application description page.";
-
+         
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
