6404643b2e4bde1ec3797d1c5d91b0a06fdb3e33
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-18 19:43:28 -0700

    added colorController for HW4

---
+++ HW4/HW4/Controllers/ColorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW4.Controllers
+{
+    public class ColorController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string first, string second)
+        {
+            @ViewBag.first = first;
+            return View();
+        }
+
+    }
+}
