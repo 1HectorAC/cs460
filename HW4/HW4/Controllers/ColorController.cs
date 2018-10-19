67611a94ae116fcf293ab705cfd1e37e4c17eeac
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-19 16:05:17 -0700

    added algorithm for mixing colors in ColorController

--- HW4/HW4/Controllers/ColorController.cs
+++ HW4/HW4/Controllers/ColorController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
+using System.Diagnostics;
+
 
 namespace HW4.Controllers
 {
@@ -18,7 +21,31 @@ namespace HW4.Controllers
         [HttpPost]
         public ActionResult Create(string first, string second)
         {
+
+            //int one = Convert.ToInt32(first.Substring(1, 2), 16);
+            //int two = Convert.ToInt32(first.Substring(3, 4), 16);
+            //int three = Convert.ToInt32(first.Substring(5, 6), 16);
+            string result = "#";
+            for(int i = 1; i <= 5; i+=2)
+            {
+                int decFirst = Convert.ToInt32(first.Substring(i, 2), 16);
+                int decSec = Convert.ToInt32(second.Substring(i, 2), 16);
+                if (decFirst + decSec > 255)
+                    result = result + "FF";
+                else
+                {
+                    if(decFirst + decSec < 10)
+                        result = result + "0" + (decFirst + decSec).ToString("X");
+                    else
+                        result = result + (decFirst + decSec).ToString("X");
+                }
+            }
+            Debug.WriteLine(first + " + " + second + " = " + result);
+
+
             @ViewBag.first = first;
+            @ViewBag.second = second;
+            @ViewBag.result = result;
             return View();
         }
 
