5c8bb5cb480a9b57766b91a7d1817a4f67e90069
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-18 17:56:35 -0700

    added calculations for converter action method in home controller

--- HW4/HW4/Controllers/HomeController.cs
+++ HW4/HW4/Controllers/HomeController.cs
@@ -17,10 +17,28 @@ namespace HW4.Controllers
         public ActionResult Converter()
         {
             string miles = (Request.QueryString["miles"]);
-            string units = (Request.QueryString["unit"]);
+            
             if(miles != null)
             {
-                string message = miles + " miles is equal to " + units;
+                string units = (Request.QueryString["unit"]);
+                double distance = 0;
+                switch (units)
+                {
+                    case "millimeters":
+                        distance = Convert.ToInt32(miles) * 1609344;
+                        break;
+                    case "centimeters":
+                        distance = Convert.ToInt32(miles) * 160934.4;
+                        break;
+                    case "meters":
+                        distance = Convert.ToInt32(miles) * 1609.344;
+                        break;
+                    case "kilometers":
+                        distance = Convert.ToInt32(miles) * 1.609344;
+                        break;
+                }
+
+                string message = miles + " miles is equal to " + distance + units;
                 ViewBag.message = message;
             }
             return View();
