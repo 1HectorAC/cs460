e736d045beac90cae0a253c0d57a894bedca6b09
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-20 15:52:22 -0700

    adjusted miles converter logic in HomeController.cs

--- HW4/HW4/Controllers/HomeController.cs
+++ HW4/HW4/Controllers/HomeController.cs
@@ -13,11 +13,16 @@ namespace HW4.Controllers
             return View();
         }
 
+        /// <summary>
+        /// This will return the Converter view and pass converted miles to it.
+        /// </summary>
+        /// <returns> Return the Converter view.</returns>
         [HttpGet]
         public ActionResult Converter()
         {
             string miles = (Request.QueryString["miles"]);
             
+            //If miles is not empty then convert to selected units.
             if(miles != null)
             {
                 string units = (Request.QueryString["unit"]);
@@ -25,20 +30,20 @@ namespace HW4.Controllers
                 switch (units)
                 {
                     case "millimeters":
-                        distance = Convert.ToInt32(miles) * 1609344;
+                        distance = Convert.ToDouble(miles) * 1609344;
                         break;
                     case "centimeters":
-                        distance = Convert.ToInt32(miles) * 160934.4;
+                        distance = Convert.ToDouble(miles) * 160934.4;
                         break;
                     case "meters":
-                        distance = Convert.ToInt32(miles) * 1609.344;
+                        distance = Convert.ToDouble(miles) * 1609.344;
                         break;
                     case "kilometers":
-                        distance = Convert.ToInt32(miles) * 1.609344;
+                        distance = Convert.ToDouble(miles) * 1.609344;
                         break;
                 }
 
-                string message = miles + " miles is equal to " + distance + units;
+                string message = miles + " miles is equal to " + distance + " " + units;
                 ViewBag.message = message;
             }
             return View();
