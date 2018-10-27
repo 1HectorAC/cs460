c2f04fcfc02d350327c8aa4157fb41e22f75f8ab
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-27 11:50:48 -0700

    added some user data annotation in TennentRequest model for hw5

--- HW5/HW5/Models/TennantRequest.cs
+++ HW5/HW5/Models/TennantRequest.cs
@@ -11,28 +11,26 @@ namespace HW5.Models
         [Key]
         public int ID { get; set; }
 
-        [Required, StringLength(20)]
+        [Required, StringLength(20), Display(Name ="First Name")]
         public string FirstName { get; set; }
 
-        [Required, StringLength(20)]
+        [Required, StringLength(20), Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required, Phone]
+        [Required, Phone, Display(Name = "Phone Number"), RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number. Use only Numbers. Format:xxx-xxx-xxxx")]
         public string PhoneNumber { get; set; }
 
-        [Required, StringLength(20)]
+        [Required, StringLength(20), Display(Name = "Apartment Name")]
         public string ApartmentName { get; set; }
 
-        [Required]
+        [Required, Display(Name = "Unit Number"), Range(0,1000)]
         public int UnitNumber { get; set; }
 
-        [Required, StringLength(100)]
+        [Required, StringLength(600), Display(Name = "Description of request")]
         public string RequestDescription { get; set; }
 
-     
         public bool AllowEnter { get; set; }
 
-        //add timestamp maker
         public DateTime RequestTimeStamp { get; set; }
         
 
