be37a4a280cee98860a7f84903ba81a740e71d96
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-10-27 14:03:41 -0700

    adjusted seeding in up.sql for hw5

--- HW5/HW5/App_Data/up.sql
+++ HW5/HW5/App_Data/up.sql
@@ -15,10 +15,10 @@ CREATE TABLE [dbo].[TennantRequests]
 );
 
 INSERT INTO [dbo].[TennantRequests] (FirstName, LastName, PhoneNumber, ApartmentName, UnitNumber, RequestDescription, RequestTimeStamp,AllowEnter) VALUES
-	('James','Jameson', '5035422345','Apartment A',4, 'Sink stopped working', '2017-01-22 11:05:44', 1),
-	('Jose','Joseson', '9711111111','Apartment B',2, 'The Regrigerator isnt working anymore and is leaking water.', '2017-05-25 10:07:08', 1),
-	('Jack','Jackson', '5034445555','Apartment C',10, 'There is a large hole in the wall. It was noticed this morning.', '2018-07-29 05:01:00', 0),
-	('Michael','Jordan', '5031234567','Apartment A',2, 'There is a bee hive inside the my room. Please come immediately because this cannot wait.', '2018-08-22 00:36:05', 0),
-	('Jim','Johnson', '5035432345','Apartment A',1, 'Neighbor tennant making too much noise at night.', '2018-08-22 00:51:10', 1),
-	('Alexandriana','Darius', '5037772635','Apartment E',7, 'The sink has stopped working again.', '2018-08-25 03:02:01', 0)
+	('James','Jameson', '503-542-2345','Apartment A',4, 'Sink stopped working', '2017-01-22 11:05:44', 1),
+	('Jose','Joseson', '971-111-1111','Apartment B',2, 'The Regrigerator isnt working anymore and is leaking water.', '2017-05-25 10:07:08', 1),
+	('Jack','Jackson', '503-444-5555','Apartment C',10, 'There is a large hole in the wall. It was noticed this morning.', '2018-07-29 05:01:00', 0),
+	('Michael','Jordan', '503-123-4567','Apartment A',2, 'There is a bee hive inside the my room. Please come immediately because this cannot wait.', '2018-08-22 00:36:05', 0),
+	('Jim','Johnson', '503-543-2345','Apartment A',1, 'Neighbor tennant making too much noise at night.', '2018-08-22 00:51:10', 1),
+	('Alexandriana','Darius', '503-777-2635','Apartment E',7, 'The sink has stopped working again.', '2018-08-25 03:02:01', 0)
 GO
