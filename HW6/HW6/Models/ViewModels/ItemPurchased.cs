34af755411141eed6b004695edae5637f8cf07a6
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-04 07:03:05 -0800

    added ItemPurchased.cs for hw6

---
+++ HW6/HW6/Models/ViewModels/ItemPurchased.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW6.Models.ViewModels
+{
+    public class ItemPurchased
+    {
+        public int StockItemID { get; set; }
+        public string Description { get; set; }
+        public decimal ProfitLine { get; set; }
+        public string SalesPerson { get; set; }
+        public ItemPurchased(int StockItemID, string Description, decimal ProfitLine, string SalesPerson)
+        {
+            this.StockItemID = StockItemID;
+            this.Description = Description;
+            this.ProfitLine = ProfitLine;
+            this.SalesPerson = SalesPerson;
+        }
+    }
+
+}
