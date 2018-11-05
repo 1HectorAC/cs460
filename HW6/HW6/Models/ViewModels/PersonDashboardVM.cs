51ec48db286d7e0c9a42bbbbf6726dc58877b7a8
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-04 16:15:33 -0800

    added annotations for ItemPurchased and PersonDashboardVM view models for hw6

--- HW6/HW6/Models/ViewModels/PersonDashboardVM.cs
+++ HW6/HW6/Models/ViewModels/PersonDashboardVM.cs
@@ -3,43 +3,57 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HW6.Models.ViewModels
 {
     public class PersonDashboardVM
     {
         //Information from the person.
+        [Display(Name="Full Name")]
         public string FullName { get; set; }
 
+        [Display(Name = "Preferred Name")]
         public string PreferredName { get; set; }
 
+        [Display(Name = "Phone")]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Fax")]
         public string FaxNumber { get; set; }
 
+        [Display(Name = "Email")]
         public string EmailAddress { get; set; }
 
+        [Display(Name = "Member Since")]
         public DateTime ValidFrom { get; set; }
 
-        //need photo
         public byte[] Photo { get; set; }
 
         //Information from the company.
+        [Display(Name = "Company")]
         public string CompanyName { get; set; }
 
+        [Display(Name = "Phone")]
         public string CompanyPhoneNumber { get; set; }
 
+        [Display(Name = "Fax")]
         public string CompanyFaxNumber { get; set; }
 
+        [Display(Name = "Website")]
         public string Website { get; set; }
 
-        public DateTime YearStarted { get; set; }
+        [Display(Name = "Member Since")]
+        public int YearStarted { get; set; }
 
         //purchases
+        [Display(Name = "Orders")]
         public int TotalOrders { get; set; }
 
+        [Display(Name = "Gross Sales")]
         public decimal GrossSales { get; set; }
 
+        [Display(Name = "Gross Profits")]
         public decimal GrossProfit { get; set; }
 
         public List<ItemPurchased> MostProfitableItems { get; set; }
