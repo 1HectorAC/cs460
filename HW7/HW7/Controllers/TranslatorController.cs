bbc70ac6d78887237974cfa1442443f295e58bf9
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-11 16:54:00 -0800

    added db stuff and basic boring word check in TranlatorController.cs for hw7

--- HW7/HW7/Controllers/TranslatorController.cs
+++ HW7/HW7/Controllers/TranslatorController.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using HW7.DAL;
+using HW7.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,39 +14,59 @@ namespace HW7.Controllers
 {
     public class TranslatorController : Controller
     {
+        private UserContext db = new UserContext();
+
         // GET: Translator
         public JsonResult Translate(string word)
         {
+            //Log user information in database.
+            User currentUser = new User
+            {
+                LogDate = DateTime.Today,
+                RequestedWord = word,
+                RequestorsIPAddress = this.Request.UserHostAddress,
+                RequestorsBrowser = this.Request.Browser.Type
+            };
 
+            db.Users.Add(currentUser);
+            db.SaveChanges();
             string gifUrl;
             bool isImage;
             //need borng definition
-            try
-            {
-                var key = System.Web.Configuration.WebConfigurationManager.AppSettings["APIKey"];
-                string source = "https://api.giphy.com/v1/stickers/translate?api_key=" + key + "&s=" + word;
-
-                WebRequest request = WebRequest.Create(@source);
-                WebResponse response = request.GetResponse();
-                Stream dataStream = response.GetResponseStream();
-
-                StreamReader reader = new StreamReader(dataStream);
-                string responseFromServer = reader.ReadToEnd();
-
-                reader.Close();
-                dataStream.Close();
-                response.Close();
-
-                JObject gifObject = JObject.Parse(responseFromServer);
-                gifUrl = gifObject["data"]["images"]["original"]["url"].ToString();
-                isImage = true;
-            }
-            catch(Exception e)
+            string [] boringWords = {"the", "is", "I", "me", "want", "are", "you", "am"};
+            if (boringWords.Contains(word))
             {
                 gifUrl = word;
                 isImage = false;
             }
+            else
+            {
+                try
+                {
+                    var key = System.Web.Configuration.WebConfigurationManager.AppSettings["APIKey"];
+                    string source = "https://api.giphy.com/v1/stickers/translate?api_key=" + key + "&s=" + word;
 
+                    WebRequest request = WebRequest.Create(@source);
+                    WebResponse response = request.GetResponse();
+                    Stream dataStream = response.GetResponseStream();
+
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = reader.ReadToEnd();
+
+                    reader.Close();
+                    dataStream.Close();
+                    response.Close();
+
+                    JObject gifObject = JObject.Parse(responseFromServer);
+                    gifUrl = gifObject["data"]["images"]["original"]["url"].ToString();
+                    isImage = true;
+                }
+                catch (Exception e)
+                {
+                    gifUrl = word;
+                    isImage = false;
+                }
+            }
             var data = new {gif = gifUrl, imageCheck = isImage};
             return Json(data, JsonRequestBehavior.AllowGet);
         }
