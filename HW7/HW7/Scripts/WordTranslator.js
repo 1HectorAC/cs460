afb71b54da0a34c328d36939c2e66fc314d241fc
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-11 16:10:25 -0800

    adjusted names of a few variables and route in WordTranslator.js for hw7

--- HW7/HW7/Scripts/WordTranslator.js
+++ HW7/HW7/Scripts/WordTranslator.js
@@ -3,40 +3,43 @@ $('#inputWords').bind('keypress', function (e) {
     //keycode 32 is for space
     if (e.keyCode == 32) {
         var words = $("#inputWords").val().toString();
-        console.log("Whole string" + words);
-        var word = words.trim().split(" ").splice(-1);
-        console.log("word to translate: " + word.toString());
-        var source = "/Translator/Translate/" + word;
-
-
-        
-        $.ajax({
-            type: "GET",
-            dataType: "json",
-            url: source,
-            success: displayWord,
-            error: errorOnAjax
-        });
+        console.log("Whole string: " + words);
+        //check for double pressing space and do nothing if so.
+        if (!words.endsWith(' ')) {
+            var word = words.trim().split(" ").splice(-1).toString();
+            console.log("word to translate: " + word.toString());
+            var source = "/Translate/" + word;
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: source,
+                success: displayWord,
+                error: detectedError
+            });
+        }
     }
 });
 
 function displayWord(data) {
-    var test = data["gif"];
+    var WordData = data["gif"];
+    //check to display image element or span element (for text)
     if (data["imageCheck"]) {
-        console.log(test);
+        console.log(WordData);
         var imageElement = document.createElement("img");
-        imageElement.src = test;
-        imageElement.alt = test;
+        imageElement.src = WordData;
+        imageElement.alt = WordData;
         imageElement.style = "width:150px;height:150px;";
         $("#Result").append(imageElement);
     }
     else {
         var textElement = document.createElement("span");
-        textElement.innerHTML = test;
+        var test2 = " " + WordData;
+        textElement.innerHTML = test2;
+        textElement.style = "font-size:30px";
         $("#Result").append(textElement);
     }
 }
 
-function errorOnAjax() {
+function detectedError() {
     console.log("error");
 }
