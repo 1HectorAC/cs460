29a104dd24441b55e4ddb5e9c6eb704b9b8b2371
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-11 16:22:18 -0800

    added down.sql and up.sql for hw7

---
+++ HW7/HW7/App_Data/up.sql
@@ -0,0 +1,18 @@
+﻿-- Users table
+
+CREATE TABLE [dbo].[Users]
+(
+	[ID]		INT IDENTITY (1,1)	NOT NULL,
+	[LogDate]	DateTime		NOT NULL,
+	[RequestedWord]	NVARCHAR(200)		NOT NULL,
+	[RequestorsIPAddress]	NVARCHAR(20)		NOT NULL,
+	[RequestorsBrowser]	NVARCHAR(30)		NOT NULL,
+
+
+	CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+INSERT INTO [dbo].[Users] (LogDate, RequestedWord, RequestorsIPAddress, RequestorsBrowser) VALUES
+	('2017-01-22 11:05:44', 'apple','10.0.0.23','Chrome'),
+	('2017-05-25 10:07:08', 'the','10.0.2.3','Safari')
+GO
