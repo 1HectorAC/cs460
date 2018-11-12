ab1b7b419a96d1058e66cc1e030b65ae815e84bd
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-11 19:45:15 -0800

    removed boring/amazing word check(will be check in js file instead) in TranslatorController.cs for hw7

--- HW7/HW7/Controllers/TranslatorController.cs
+++ HW7/HW7/Controllers/TranslatorController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,7 +17,11 @@ namespace HW7.Controllers
     {
         private UserContext db = new UserContext();
 
-        // GET: Translator
+        /// <summary>
+        /// This action method takes in a word and tranlate it to a gif representation of it.
+        /// </summary>
+        /// <param name="word">This is the word that needs to be tranlated.</param>
+        /// <returns>It returns a JsonResult object which contains the gif url link.</returns>
         public JsonResult Translate(string word)
         {
             //Log user information in database.
@@ -27,47 +32,38 @@ namespace HW7.Controllers
                 RequestorsIPAddress = this.Request.UserHostAddress,
                 RequestorsBrowser = this.Request.Browser.Type
             };
-
             db.Users.Add(currentUser);
             db.SaveChanges();
+
+            //This will hold the gif url for the tranlated word.
             string gifUrl;
-            bool isImage;
-            //need borng definition
-            string [] boringWords = {"the", "is", "I", "me", "want", "are", "you", "am"};
-            if (boringWords.Contains(word))
+
+            //This is part is retriving information from the api.
+            try
+            {
+                var key = System.Web.Configuration.WebConfigurationManager.AppSettings["APIKey"];
+                string source = "https://api.giphy.com/v1/stickers/translate?api_key=" + key + "&s=" + word;
+
+                WebRequest request = WebRequest.Create(@source);
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                JObject gifObject = JObject.Parse(responseFromServer);
+                gifUrl = gifObject["data"]["images"]["original"]["url"].ToString();
+            }
+            catch (Exception e)
             {
                 gifUrl = word;
-                isImage = false;
             }
-            else
-            {
-                try
-                {
-                    var key = System.Web.Configuration.WebConfigurationManager.AppSettings["APIKey"];
-                    string source = "https://api.giphy.com/v1/stickers/translate?api_key=" + key + "&s=" + word;
 
-                    WebRequest request = WebRequest.Create(@source);
-                    WebResponse response = request.GetResponse();
-                    Stream dataStream = response.GetResponseStream();
-
-                    StreamReader reader = new StreamReader(dataStream);
-                    string responseFromServer = reader.ReadToEnd();
-
-                    reader.Close();
-                    dataStream.Close();
-                    response.Close();
-
-                    JObject gifObject = JObject.Parse(responseFromServer);
-                    gifUrl = gifObject["data"]["images"]["original"]["url"].ToString();
-                    isImage = true;
-                }
-                catch (Exception e)
-                {
-                    gifUrl = word;
-                    isImage = false;
-                }
-            }
-            var data = new {gif = gifUrl, imageCheck = isImage};
+            var data = new { gif = gifUrl };
             return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
