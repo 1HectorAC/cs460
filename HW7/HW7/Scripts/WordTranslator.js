0c452fc97155c33827309c321df669d0aff7a34d
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-11 22:54:51 -0800

    added check for boring/amazing word in WordTranlator.js for hw7

--- HW7/HW7/Scripts/WordTranslator.js
+++ HW7/HW7/Scripts/WordTranslator.js
@@ -4,18 +4,35 @@ $('#inputWords').bind('keypress', function (e) {
     if (e.keyCode == 32) {
         var words = $("#inputWords").val().toString();
         console.log("Whole string: " + words);
-        //check for double pressing space and do nothing if so.
+        //check for double space and do nothing if so.
         if (!words.endsWith(' ')) {
             var word = words.trim().split(" ").splice(-1).toString();
             console.log("word to translate: " + word.toString());
-            var source = "/Translate/" + word;
-            $.ajax({
-                type: "GET",
-                dataType: "json",
-                url: source,
-                success: displayWord,
-                error: detectedError
-            });
+
+            //check for boring or amazing words
+            var commonWords = ["am", "as", "are", "and", "are", "at", "be", "for", "from", "have", "he", "his", "i", "in", "is", "it", "me", "of", "on", "or", "she", "the", "that", "they", "this", "to", "want", "was", "with", "you"];
+            var wordLowerCased = word.toLowerCase();
+            //check if not a common word or if not a number
+            if (commonWords.indexOf(wordLowerCased) == -1 || !isNaN(word)) {
+                //translate amazing word
+                var source = "/Translate/" + word;
+                $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                    url: source,
+                    success: displayWord,
+                    error: detectedError
+                });
+            }
+            else {
+                //display boring word
+                console.log("boring check works");
+                var textElement = document.createElement("span");
+                var wordWithSpace = " " + word;
+                textElement.innerHTML = wordWithSpace;
+                textElement.style = "font-size:30px";
+                $("#Result").append(textElement);
+            }
         }
     }
 });
@@ -23,21 +40,13 @@ $('#inputWords').bind('keypress', function (e) {
 function displayWord(data) {
     var WordData = data["gif"];
     //check to display image element or span element (for text)
-    if (data["imageCheck"]) {
-        console.log(WordData);
-        var imageElement = document.createElement("img");
-        imageElement.src = WordData;
-        imageElement.alt = WordData;
-        imageElement.style = "width:150px;height:150px;";
-        $("#Result").append(imageElement);
-    }
-    else {
-        var textElement = document.createElement("span");
-        var test2 = " " + WordData;
-        textElement.innerHTML = test2;
-        textElement.style = "font-size:30px";
-        $("#Result").append(textElement);
-    }
+    console.log(WordData);
+    var imageElement = document.createElement("img");
+    imageElement.src = WordData;
+    imageElement.alt = WordData;
+    imageElement.style = "width:150px;height:150px;";
+    $("#Result").append(imageElement);
+
 }
 
 function detectedError() {
