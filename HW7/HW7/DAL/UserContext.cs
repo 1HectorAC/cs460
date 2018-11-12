66e7cdba98adf817554a6a904a60a68f235990ac
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-11 16:39:46 -0800

    added UserContext.cs for hw7

---
+++ HW7/HW7/DAL/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using HW7.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HW7.DAL
+{
+    public class UserContext : DbContext
+    {
+        /// <summary>
+        /// Contructor for UserContext. Has database Connection.
+        /// </summary>
+        public UserContext() : base("name=HW7UserLog")
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
