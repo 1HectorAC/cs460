2284fab7c72cfe01266047d0a5347540330027ba
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-15 19:47:51 -0800

    added HW8 project

---
+++ HW8/HW8/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using HW8.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW8.Controllers
+{
+    public class HomeController : Controller
+    {
+        AuctionContext db = new AuctionContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListItems()
+        {
+            return View(db.Items);
+
+        }
+
+        public ActionResult CreateItem()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateItem([Bind(Include = "ItemID, Name, Description, SellerID")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("ListItems");
+            }
+
+            return View(item);
+        }
+
+        public ActionResult DetailItem(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(item);
+        }
+
+
+
+        // GET: Users/Edit/5
+        public ActionResult EditItem(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditItem([Bind(Include = "ItemID,Name,Description,SellerID")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult DeleteItem(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("DeleteItem")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteItemConfirmed(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+
+}
---
+++ HW8/HW8/DAL/AuctionContext.cs
@@ -0,0 +1,58 @@
+namespace HW8.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AuctionContext : DbContext
+    {
+        public AuctionContext()
+            : base("name=AuctionContext")
+        {
+        }
+
+        public virtual DbSet<Bid> Bids { get; set; }
+        public virtual DbSet<Buyer> Buyers { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<Seller> Sellers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bid>()
+                .Property(e => e.Price)
+                .HasPrecision(18, 0);
+
+            modelBuilder.Entity<Buyer>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Buyer>()
+                .HasMany(e => e.Bids)
+                .WithRequired(e => e.Buyer)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Item>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Item>()
+                .Property(e => e.Description)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Item>()
+                .HasMany(e => e.Bids)
+                .WithRequired(e => e.Item)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Seller>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Seller>()
+                .HasMany(e => e.Items)
+                .WithRequired(e => e.Seller)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
