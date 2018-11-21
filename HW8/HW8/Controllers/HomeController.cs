8c558873463290772c6c4556db43983396527581
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2018-11-21 09:00:19 -0800

    added a bunch of comments to HomeController.cs for hw8

--- HW8/HW8/Controllers/HomeController.cs
+++ HW8/HW8/Controllers/HomeController.cs
@@ -15,27 +15,44 @@ namespace HW8.Controllers
     {
         AuctionContext db = new AuctionContext();
 
+        /// <summary>
+        /// Get index View and pass it a list of recent 10 bids from Bids.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             ViewBag.TodayDate = DateTime.Today.ToString("d");
             return View(db.Bids.OrderByDescending(n => n.TimeStamp).Take(10).ToList());
         }
 
+        /// <summary>
+        /// Get the ListItems view and pass it a list of all the Items.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ListItems()
         {
             return View(db.Items.ToList());
-
         }
 
+        /// <summary>
+        /// Use GET and return createItem View.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult CreateItem()
         {
             return View(db.Sellers);
         }
+
+        /// <summary>
+        /// Use Post and Return a View. This will also add changes to database.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult CreateItem([Bind(Include = " Name, Description, SellerID")] Item item)
         {
-            
+
             if (ModelState.IsValid)
             {
                 db.Items.Add(item);
@@ -43,10 +60,14 @@ namespace HW8.Controllers
                 return RedirectToAction("ListItems");
             }
 
-
             return View(db.Sellers);
         }
 
+        /// <summary>
+        /// Get the DetailItem View. This will also pass an item of an item based on the id passed to it.
+        /// </summary>
+        /// <param name="id">The id of an Item.</param>
+        /// <returns></returns>
         public ActionResult DetailItem(int? id)
         {
             if (id == null)
@@ -58,13 +79,14 @@ namespace HW8.Controllers
             {
                 return HttpNotFound();
             }
-            
             return View(item);
         }
 
-
-
-        // GET:
+        /// <summary>
+        /// Use GET and return a EditItem View.
+        /// </summary>
+        /// <param name="id">The id of an item in Items.</param>
+        /// <returns></returns>
         public ActionResult EditItem(int? id)
         {
             if (id == null)
@@ -79,9 +101,11 @@ namespace HW8.Controllers
             return View(item);
         }
 
-
-
-        // POST:
+        /// <summary>
+        /// Use POST and return a EditItem View. This will save changes made to an Item in Items.
+        /// </summary>
+        /// <param name="item">The item that will be edited.</param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult EditItem([Bind(Include = "ItemID,Name,Description,SellerID")] Item item)
@@ -95,7 +119,11 @@ namespace HW8.Controllers
             return View(item);
         }
 
-        // GET:
+        /// <summary>
+        /// This will use GET and return an item based on the id passed to it.
+        /// </summary>
+        /// <param name="id">The id of an item in Items.</param>
+        /// <returns></returns>
         public ActionResult DeleteItem(int? id)
         {
             if (id == null)
@@ -110,7 +138,11 @@ namespace HW8.Controllers
             return View(item);
         }
 
-        // POST: 
+        /// <summary>
+        /// This will use Post and return a DeleteItemConfimed View. This actually remove an item from Items.
+        /// </summary>
+        /// <param name="id">The id of an item from Items.</param>
+        /// <returns></returns>
         [HttpPost, ActionName("DeleteItem")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteItemConfirmed(int id)
@@ -121,6 +153,10 @@ namespace HW8.Controllers
             return RedirectToAction("ListItems");
         }
 
+        /// <summary>
+        /// This is involved in deletion.
+        /// </summary>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -130,6 +166,10 @@ namespace HW8.Controllers
             base.Dispose(disposing);
         }
 
+        /// <summary>
+        /// This will use GET and return a CreateBid View.
+        /// </summary>
+        /// <returns>CreateBid View</returns>
         public ActionResult CreateBid()
         {
             BuyerItem BuyersAndItems = new BuyerItem
@@ -140,6 +180,12 @@ namespace HW8.Controllers
 
             return View(BuyersAndItems);
         }
+
+        /// <summary>
+        /// This will use Post and will either redirect or return a CreateBid View.
+        /// </summary>
+        /// <param name="bid">This is a binded bid that will be added to Bids</param>
+        /// <returns>CreateBid View.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult CreateBid([Bind(Include = "ItemID,BuyerID,Price")] Bid bid)
@@ -160,14 +206,17 @@ namespace HW8.Controllers
             return View(BuyersAndItems);
         }
 
-            public JsonResult ItemBids(int? id)
+        /// <summary>
+        /// This will create a var object and store the bids of a specified item (only the information that is needed) and then return a JsonResult with the infomation.
+        /// </summary>
+        /// <param name="id">The id of an Item in Items.</param>
+        /// <returns>A JsonResult.</returns>
+        public JsonResult ItemBids(int? id)
         {
 
-            //var data = db.Items.Find(id).Bids.OrderByDescending(n => n.Price).Select(n => new { TablePrice = n.Price, TableBuyer = n.Buyer.Name }).ToList();
             var test = db.Items.Find(id).Bids.OrderByDescending(n => n.Price).Select(n => new { TablePrice = n.Price, TableBuyer = n.Buyer.Name }).ToList();
-            var data = new {TablePrice = test.Select(n=>n.TablePrice), TableBuyer = test.Select(n=>n.TableBuyer) };
+            var data = new { TablePrice = test.Select(n => n.TablePrice), TableBuyer = test.Select(n => n.TableBuyer) };
 
-            //var data = new {fewf = 4 };
             return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
